6bb20faa02eb88c04728e33d709c77aa8e1fd08b
niaiko <niaikorakoto@yahoo.com>
2021-11-28 12:29:49 +0300

    server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-console.log("Start server ***")
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/<fin-corp>'));
-console.log("**** runing server ****")
+
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+
-    '/dist/<fin-corp>/index.html'));});
-    app.listen(process.env.PORT || 8080);
-console.log("success")
+    
+res.sendFile(path.join(__dirname+'/dist/<fin-corp>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
